52b66197a181c67b90f444d505ca9da33cecc9eb
younes1233 <younesossman12@gmail.com>
2024-05-18 12:38:52 -0700

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My One-Page React App</h1>
+        <p>
+          This is a simple one-page application built with React.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
